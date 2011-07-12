e1db523ae57ca23b49f0dda1bd5002ee35a74acf
Peter Keating <petemoov2@gmail.com>
2011-07-12 18:02:29 +0100

    added BusinessLogic project.

---
+++ MvcTurbineAreas.BusinessLogic/ExampleDependency.cs
@@ -0,0 +1,15 @@
+﻿namespace MvcTurbineAreas.BusinessLogic
+{
+    public class ExampleDependency : IExampleDependency
+    {
+        public string MyMethod()
+        {
+            return "Hello World!";
+        }
+    }
+
+    public interface IExampleDependency
+    {
+        string MyMethod();
+    }
+}
